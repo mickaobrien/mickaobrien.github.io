e5eb355924ee153b45b15d635d3bdf01fcb10bb6
Mick O'Brien <mickaobrien@gmail.com>
2014-02-14 18:18:11 +0000

    NewsNerdJobs initial commit

---
+++ newsnerdjobs/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: [
+                    'js/*.js'
+                ],
+                dest: 'production.js'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'production.js',
+                dest: 'production.min.js'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js', 'test.html', 'jobs.css'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+};
---
+++ newsnerdjobs/js/getdata.js
@@ -0,0 +1,39 @@
+var KEY = '0AmqohgGX3YQadE1VSktrWG1nNFF6RUFNT1RKa0k0a2c',
+    ALL_DATA;
+function init() {
+    Tabletop.init({key: KEY,
+                   callback: processData,
+                   simpleSheet: true, 
+                   wanted: ['Listings'] 
+        } 
+    )
+}
+
+function processData(data, tabletop) {
+    ALL_DATA = data;
+    var source = $('#jobs').html(),
+        template = Handlebars.compile(source);
+
+    $('#jobs-list').append(template({"jobs":ALL_DATA}));
+}
+
+function IsURL(url) {
+
+    var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+        + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+        + "|" // 允许IP和DOMAIN（域名）
+        + "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+        + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+        + "[a-z]{2,6})" // first level domain- .com or .museum
+        + "(:[0-9]{1,4})?" // 端口- :80
+        + "((/?)|" // a slash isn't required if there is no file name
+        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+     var re=new RegExp(strRegex);
+     return re.test(url);
+ }
+
+function isUrl(s) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)? (\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return regexp.test(s);
+}
