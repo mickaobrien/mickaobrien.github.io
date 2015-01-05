b0018666d18cf7848c13334dd3ef1c92c047abe7
Mick O'Brien <mickaobrien@gmail.com>
2015-01-05 11:46:43 +0000

    Fix whitespace

--- newsnerdjobs/js/getdata.js
+++ newsnerdjobs/js/getdata.js
@@ -2,10 +2,10 @@ var KEY = '0AmqohgGX3YQadE1VSktrWG1nNFF6RUFNT1RKa0k0a2c';
 
 function urlify(text) {
     //Replace any URL in text with a link
-        var urlRegex = /(https?:\/\/[^\s]+)/g;
-        return text.replace(urlRegex, function(url) {
-                    return '<a href="' + url + '">' + url + '</a>';
-        })
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '">' + url + '</a>';
+    })
 }
 
 
@@ -27,13 +27,13 @@ function init() {
     });
 
     Tabletop.init({key: KEY,
-                   callback: processData,
-                   simpleSheet: true, 
-                   wanted: ['Listings'],
-                   orderby: 'dateentered',
-                   reverse: true
-        } 
-    );
+                  callback: processData,
+                  simpleSheet: true, 
+                  wanted: ['Listings'],
+                  orderby: 'dateentered',
+                  reverse: true
+    } 
+                 );
 }
 
 function processData(data, tabletop) {
@@ -46,18 +46,18 @@ function processData(data, tabletop) {
 function IsURL(url) {
 
     var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
-        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
-        + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
-        + "|" // 允许IP和DOMAIN（域名）
-        + "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
-        + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
-        + "[a-z]{2,6})" // first level domain- .com or .museum
-        + "(:[0-9]{1,4})?" // 端口- :80
-        + "((/?)|" // a slash isn't required if there is no file name
-        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
-     var re=new RegExp(strRegex);
-     return re.test(url);
- }
+    + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+    + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+    + "|" // 允许IP和DOMAIN（域名）
+    + "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+    + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+    + "[a-z]{2,6})" // first level domain- .com or .museum
+    + "(:[0-9]{1,4})?" // 端口- :80
+    + "((/?)|" // a slash isn't required if there is no file name
+    + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+    var re=new RegExp(strRegex);
+    return re.test(url);
+}
 
 function isUrl(s) {
     var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)? (\/|\/([\w#!:.?+=&%@!\-\/]))?/
